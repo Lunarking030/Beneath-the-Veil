b92cf43d199afe03076f69dc7ebe621fb136092d
TLuu52 <luuthuan817@gmail.com>
2023-04-13 17:14:08 -0400

    added options menu items and pause menu
    added stuff to options and added pause menu

--- Beneath_The_Veil_GameProject/Assets/Scripts/SettingsMenu.cs
+++ Beneath_The_Veil_GameProject/Assets/Scripts/SettingsMenu.cs
@@ -1,18 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
 
 public class SettingsMenu : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+   public AudioMixer audioMixer;
 
-    // Update is called once per frame
-    void Update()
+   public void SetVolume (float volume)
     {
-        
+        audioMixer.SetFloat("Volume", volume);
     }
 }
