27a0789e1c8091c94287602be4b535dbe69b25d6
TLuu52 <luuthuan817@gmail.com>
2023-04-17 19:19:15 -0400

    fixed the main menu up a bit

--- Beneath_The_Veil_GameProject/Assets/Scripts/GameManager.cs
+++ Beneath_The_Veil_GameProject/Assets/Scripts/GameManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
@@ -8,7 +9,18 @@ public class GameManager : MonoBehaviour
     public GameObject Axe;
     public GameObject Sword;
     public GameObject Staff;
-    
+
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("QUIT!");
+        Application.Quit();
+    }
+
     // Start is called before the first frame update
     void Start()
     {
