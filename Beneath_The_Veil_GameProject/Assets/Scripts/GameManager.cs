e22453ba6e9f988f36166df1e641851ebcb2a70d
TLuu52 <luuthuan817@gmail.com>
2023-04-17 19:00:54 -0400

    Fixing the merge with project

--- Beneath_The_Veil_GameProject/Assets/Scripts/GameManager.cs
+++ Beneath_The_Veil_GameProject/Assets/Scripts/GameManager.cs
@@ -1,7 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
@@ -9,20 +8,7 @@ public class GameManager : MonoBehaviour
     public GameObject Axe;
     public GameObject Sword;
     public GameObject Staff;
-
-
-    public void PlayGame()
-    {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
-    }
-
-    public void QuitGame()
-    {
-        Debug.Log("QUIT!");
-        Application.Quit();
-    }
-
-
+    
     // Start is called before the first frame update
     void Start()
     {
