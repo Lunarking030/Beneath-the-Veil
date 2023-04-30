853a4aec0e4802554576bc586c0fd83110dac97e
John-Bryan Nicdao <nicdj@farmingdale.edu>
2023-04-30 12:06:07 -0400

    Cloned project
    Project has been cloned and updated to current progress.

---
+++ Assets/Scripts/LevelLoader.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class LevelLoader : MonoBehaviour
+{
+
+
+    public GameObject LoadingScreen;
+    public Slider slider;
+    public Text ProgressText;
+
+
+    public void LoadLevel(int sceneIndex)
+    {
+        StartCoroutine(LoadAsynchronously(sceneIndex));
+        
+    }
+
+    IEnumerator LoadAsynchronously(int sceneIndex)
+    {
+        AsyncOperation operation = SceneManager.LoadSceneAsync(sceneIndex);
+
+
+        LoadingScreen.SetActive(true);
+
+
+        while (!operation.isDone)
+        {
+            float progress = Mathf.Clamp01(operation.progress / .9f);
+            
+            slider.value = progress;
+            ProgressText.text = progress * 100f + "%";
+
+            yield return null;
+        }
+    }
+
+
+}
