d44e5d7b9ecf8c08a0b5a235ee288fbf9c413785
John-Bryan Nicdao <nicdj@farmingdale.edu>
2023-05-01 11:13:20 -0400

    Added Enemy AI
    New enemy AI script and applied it to magician lady for testing. It works, needs further testing and tweaking.

--- Assets/Enemies/Magician_Lady/Scripts/EnemyAI.cs
+++ Assets/Enemies/Magician_Lady/Scripts/EnemyAI.cs
@@ -37,9 +37,53 @@ public class EnemyAI : MonoBehaviour
         playerInSightRange = Physics.CheckSphere(transform.position, sightRange, whatIsPlayer);
         playerInAttackRange = Physics.CheckSphere(transform.position, attackRange, whatIsPlayer);
 
+<<<<<<< Updated upstream
         if (!playerInSightRange && !playerInAttackRange) Patroling();
         if (playerInSightRange && !playerInAttackRange) ChasePlayer();
         if (playerInAttackRange && playerInSightRange) AttackPlayer();
+=======
+        if (!m_IsPatrol)
+        {
+            Chasing();
+        }
+        else
+        {
+            Patroling();
+        }
+    }
+
+    private void Chasing()
+    {
+        //  The enemy is chasing the player
+        m_PlayerNear = false;                       //  Set false that the player is near beacause the enemy already sees the player
+        playerLastPosition = Vector3.zero;          //  Reset the player near position
+
+        if (!m_CaughtPlayer)
+        {
+            Move(speedRun);
+            navMeshAgent.SetDestination(m_PlayerPosition);          //  set the destination of the enemy to the player location
+        }
+        if (navMeshAgent.remainingDistance <= navMeshAgent.stoppingDistance)    //  Control if the enemy arrive to the player location
+        {
+            if (m_WaitTime <= 0 && !m_CaughtPlayer && Vector3.Distance(transform.position, GameObject.FindGameObjectWithTag("Player").transform.position) >= 6f)
+            {
+                //  Check if the enemy is not near to the player, returns to patrol after the wait time delay
+                m_IsPatrol = true;
+                m_PlayerNear = false;
+                Move(speedWalk);
+                m_TimeToRotate = timeToRotate;
+                m_WaitTime = startWaitTime;
+                navMeshAgent.SetDestination(waypoints[m_CurrentWaypointIndex].position);
+            }
+            else
+            {
+                if (Vector3.Distance(transform.position, GameObject.FindGameObjectWithTag("Player").transform.position) >= 2.5f)
+                    //  Wait if the current position is not the player position
+                    Stop();
+                m_WaitTime -= Time.deltaTime;
+            }
+        }
+>>>>>>> Stashed changes
     }
 
     private void Patroling()
@@ -111,9 +155,89 @@ public class EnemyAI : MonoBehaviour
 
     private void OnDrawGizmosSelected()
     {
+<<<<<<< Updated upstream
         Gizmos.color = Color.red;
         Gizmos.DrawWireSphere(transform.position, attackRange);
         Gizmos.color = Color.yellow;
         Gizmos.DrawWireSphere(transform.position, sightRange);
+=======
+        navMeshAgent.isStopped = true;
+        navMeshAgent.speed = 0;
+        gameObject.GetComponent<Animator>().Play("Idle");
+    }
+
+    void Move(float speed)
+    {
+        navMeshAgent.isStopped = false;
+        navMeshAgent.speed = speed;
+    }
+
+    void CaughtPlayer()
+    {
+        m_CaughtPlayer = true;
+    }
+
+    void LookingPlayer(Vector3 player)
+    {
+        navMeshAgent.SetDestination(player);
+        if (Vector3.Distance(transform.position, player) <= 0.3)
+        {
+            if (m_WaitTime <= 0)
+            {
+                m_PlayerNear = false;
+                Move(speedWalk);
+                navMeshAgent.SetDestination(waypoints[m_CurrentWaypointIndex].position);
+                m_WaitTime = startWaitTime;
+                m_TimeToRotate = timeToRotate;
+            }
+            else
+            {
+                Stop();
+                m_WaitTime -= Time.deltaTime;
+            }
+        }
+    }
+
+    void EnviromentView()
+    {
+        Collider[] playerInRange = Physics.OverlapSphere(transform.position, viewRadius, playerMask);   //  Make an overlap sphere around the enemy to detect the playermask in the view radius
+
+        for (int i = 0; i < playerInRange.Length; i++)
+        {
+            Transform player = playerInRange[i].transform;
+            Vector3 dirToPlayer = (player.position - transform.position).normalized;
+            if (Vector3.Angle(transform.forward, dirToPlayer) < viewAngle / 2)
+            {
+                float dstToPlayer = Vector3.Distance(transform.position, player.position);          //  Distance of the enemy and the player
+                if (!Physics.Raycast(transform.position, dirToPlayer, dstToPlayer, obstacleMask))
+                {
+                    m_playerInRange = true;             //  The player has been seeing by the enemy and then the nemy starts to chasing the player
+                    m_IsPatrol = false;                 //  Change the state to chasing the player
+                }
+                else
+                {
+                    /*
+                     *  If the player is behind a obstacle the player position will not be registered
+                     * */
+                    m_playerInRange = false;
+                }
+            }
+            if (Vector3.Distance(transform.position, player.position) > viewRadius)
+            {
+                /*
+                 *  If the player is further than the view radius, then the enemy will no longer keep the player's current position.
+                 *  Or the enemy is a safe zone, the enemy will no chase
+                 * */
+                m_playerInRange = false;                //  Change the sate of chasing
+            }
+            if (m_playerInRange)
+            {
+                /*
+                 *  If the enemy no longer sees the player, then the enemy will go to the last position that has been registered
+                 * */
+                m_PlayerPosition = player.transform.position;       //  Save the player's current position if the player is in range of vision
+            }
+        }
+>>>>>>> Stashed changes
     }
 }
