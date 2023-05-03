df7f1fc9c0fe8d9eec59aae397c76ea08decca75
TLuu52 <luuthuan817@gmail.com>
2023-05-03 13:00:17 -0400

    Added health bars to the enemis and updated enemy health script

--- Assets/Scripts/EnemyHealth.cs
+++ Assets/Scripts/EnemyHealth.cs
@@ -1,16 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class EnemyHealth : MonoBehaviour
 {
     public int maxHealth = 100;
     public float regenRate = 1.0f; // Health regenerated per second
     private int currentHealth;
+    public Slider healthSlider;
 
     private void Start()
     {
         currentHealth = maxHealth;
+        healthSlider.maxValue = maxHealth;
+        healthSlider.value = currentHealth;
     }
 
     private void Update()
@@ -19,12 +23,14 @@ public class EnemyHealth : MonoBehaviour
         {
             currentHealth += Mathf.RoundToInt(regenRate * Time.deltaTime);
             currentHealth = Mathf.Clamp(currentHealth, 0, maxHealth);
+            healthSlider.value = currentHealth;
         }
     }
 
     public void TakeDamage(int damage)
     {
         currentHealth -= damage;
+        healthSlider.value = currentHealth;
 
         if (currentHealth <= 0)
         {
