9c34993ec994ba752624a18596806cdf0393ad14
John-Bryan Nicdao <nicdj@farmingdale.edu>
2023-05-07 16:23:35 -0400

    Updated enemyMove script

--- Assets/Enemies/Magician_Lady/Scripts/EnemyMove.cs
+++ Assets/Enemies/Magician_Lady/Scripts/EnemyMove.cs
@@ -9,36 +9,48 @@ public class EnemyMove : MonoBehaviour
     private Transform player;
     private NavMeshAgent agent;
     public float enemyDistance = 0.7f;
-
-
+    public LayerMask obstacleLayer; // Layer mask for obstacles
 
     private void Start()
     {
-      
         player = GameObject.FindWithTag("Player").transform;
-
         agent = GetComponent<NavMeshAgent>();
-
-       
+        agent.updateRotation = false; // Disable NavMeshAgent rotation so we can control it ourselves
     }
 
-    // Update is called once per frame
     void Update()
     {
-
-        //Look at the player
+        // Look at the player
         transform.LookAt(player);
 
+        // Set the NavMeshAgent's destination
         agent.SetDestination(player.transform.position);
-        
 
+        // Check if the player is within attacking distance
         if (Vector3.Distance(transform.position, player.position) < enemyDistance)
         {
-            gameObject.GetComponent<NavMeshAgent>().velocity = Vector3.zero;
-            gameObject.GetComponent<Animator>().Play("Attack");
+            agent.velocity = Vector3.zero;
+            GetComponent<Animator>().Play("Attack");
         }
 
+        // Check for obstacles in front of the enemy and move around them
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, transform.forward, out hit, agent.radius, obstacleLayer))
+        {
+            // Calculate a new destination to move around the obstacle
+            Vector3 newDestination = transform.position + transform.right * 3f;
+            NavMeshHit navHit;
+            if (NavMesh.SamplePosition(newDestination, out navHit, 5f, NavMesh.AllAreas))
+            {
+                agent.SetDestination(navHit.position);
+            }
+        }
 
-
+        // Update the enemy's rotation to face its movement direction
+        if (agent.velocity.magnitude > 0)
+        {
+            transform.rotation = Quaternion.LookRotation(agent.velocity.normalized);
+        }
     }
 }
+
